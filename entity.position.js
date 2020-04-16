291a0e88ef2274e403c394399ec824d531c58026
marchuanv <marchuanv@gmail.com>
2020-04-16 16:37:17 +0200

    entity work

--- entity.position.js
+++ entity.position.js
@@ -3,10 +3,13 @@ const utils = require("utils");
 
 const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 4000;
+
+const publicHost = process.env.PUB_HOST || "localhost";
 const publicPort = process.env.PUB_PORT || 4000;
 
-const destHost = "relayor.herokuapp.com";
-const destPort = 80;
+const destHost = process.env.DEST_HOST || "localhost";
+const destPort = process.env.DEST_PORT || 5000;
+
 const path = "/entity/position";
 const broadcastPath = "/broadcast";
 const registerPath = "/register";
@@ -14,14 +17,14 @@ const contentType = "application/json";
 const username = process.env.PLAYERNAME || "anonymous";
 const passphrase = process.env.PASSPHRASE || "secure1";
 
-messagebus.subscribe({ host, port: port, path, contentType }).callback = (entity) => {
-    const message = `${host}:${port} received response`;
+messagebus.subscribe({ host, port, path, contentType }).callback = (entity) => {
+    const message = `${publicHost}:${publicPort} received response`;
     utils.log("Entity Position",`${message}: `, entity);
     return message;
 }
 
 (async () => {
     const entity = {name: username, position: { x:0, y:0, z:0 }};
-    await messagebus.publish({ username, passphrase, host: destHost, port: destPort, path: registerPath, contentType, content: { host,  port: publicPort, path }});
+    await messagebus.publish({ username, passphrase, host: destHost, port: destPort, path: registerPath, contentType, content: { host: publicHost,  port: publicPort, path }});
     await messagebus.publish({ username, passphrase, host: destHost, port: destPort, path: broadcastPath, contentType, content: { path, contentType, content: entity }});
 })();
