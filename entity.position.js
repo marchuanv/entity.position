97ba8c716760ec3105b32308ef0c80218611f849
marchuanv <marchuanv@gmail.com>
2020-04-30 23:51:41 +0200

    refactor

--- entity.position.js
+++ entity.position.js
@@ -1,11 +1,11 @@
 const messagebus = require("messagebus");
 const fs = require("fs");
 
-const privatePort = Number(process.env.PORT || 4000);
-const publicHost = process.env.PUB_HOST || "localhost";
-const publicPort = Number(process.env.PUB_PORT || privatePort);
+const sourcePrivatePort = Number(process.env.PORT || 4000);
+const sourcePublicHost = process.env.PUB_HOST || "localhost";
+const sourcePublicPort = Number(process.env.PUB_PORT || sourcePrivatePort);
 
-const broadcastHost = process.env.BROADCAST_HOST || publicHost;
+const broadcastHost = process.env.BROADCAST_HOST || sourcePublicHost;
 const broadcastPort = Number(process.env.BROADCAST_PORT || 5000);
 
 const broadcastPath = "/broadcast";
@@ -27,20 +27,42 @@ logging.config([
 
 (async () => {
 
-    messagebus.subscribe({ publicHost, publicPort, privatePort, path: "/remote/move", contentType }).callback = (entity) => {
+    messagebus.subscribe({ sourcePublicHost, sourcePublicPort, sourcePrivatePort, path: "/remote/move", contentType }).callback = (entity) => {
         logging.write("Entity Position",`remote entity ${entity.name} has moved`, entity.position);
     }
 
-    messagebus.subscribe({ publicHost, publicPort, privatePort, path: "/local/move", contentType }).callback = async (entity) => {
+    messagebus.subscribe({ sourcePublicHost, sourcePublicPort, sourcePrivatePort, path: "/local/move", contentType }).callback = async (entity) => {
+        
         logging.write("Entity Position",`local entity ${entity.name} has moved`, entity.position);
-        await messagebus.publish({ username, passphrase, publicHost: broadcastHost, publicPort: broadcastPort, path: broadcastPath, contentType, content: { path: "/remote/move", contentType, content: entity }});
+        
+        await messagebus.publish({ 
+            username, 
+            passphrase, 
+            destPublicHost: broadcastHost, 
+            destPublicPort: broadcastPort,
+            sourcePublicHost, 
+            sourcePublicPort,
+            path: registerPath, 
+            contentType, 
+            content: { host: sourcePublicHost,  port: sourcePublicPort, path: "/remote/move" }
+        });
+    
+        await messagebus.publish({ 
+            username, 
+            passphrase, 
+            destPublicHost: broadcastHost, 
+            destPublicPort: broadcastPort,
+            sourcePublicHost, 
+            sourcePublicPort,
+            path: broadcastPath, 
+            contentType, 
+            content: { path: "/remote/move", contentType, content: entity }
+        });
     }
 
-    messagebus.subscribe({ publicHost, publicPort, privatePort, path: "/", contentType: "text/html", isSecure: false }).callback = async () => {
+    messagebus.subscribe({ sourcePublicHost, sourcePublicPort, sourcePrivatePort, path: "/", contentType: "text/html", isSecure: false }).callback = async () => {
         return fs.readFileSync("./entity.position.html","utf8");
     }
-
-    await messagebus.publish({ username, passphrase, publicHost: broadcastHost, publicPort: broadcastPort, path: registerPath, contentType, content: { host: publicHost,  port: publicPort, path: "/remote/move" }});
 
 })().catch((err)=>{
     console.log(err);
